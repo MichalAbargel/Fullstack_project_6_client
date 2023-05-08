1948c3025ee59069317d1c935556bef70f31a368
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-05-08 13:55:32 +0300

    error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import Login from "./Login";
 import UserDashboard from "./UserDashBoard";
+import Error from "./Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <UserDashboard userId={1} />;
+  return (
+    <BrowserRouter>
+      <Routes location="/login">
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<UserDashboard userId={1} />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
