4863389b06ea6103e75f8e11999b9db472435574
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-05-10 16:37:57 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import "./App.css";
+/*import "./App.css";
 import Login from "./Login";
 import UserDashboard from "./UserDashBoard";
 import Error from "./Error";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  //return <UserDashboard userId={1} />;
   return (
     <BrowserRouter>
-      <Routes location="/login">
+      <Routes>
+       
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<UserDashboard userId={1} />} />
+        <Route path="/users/:userid" element={<div>users</div>}>
+          
+          <Route path="*" element={<div>user *</div>} />
+          <Route path="info" element={<div>info</div>} />
+          <Route path="todos/:id" element={<div>todos</div>} />
+          <Route path="posts/:id" element={<div>posts</div>} />
+          <Route path="albums/:id" element={<div>albums</div>} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;*/
+
+import "./App.css";
+import Login from "./Login";
+import UserDashboard from "./UserDashBoard";
+import Error from "./Error";
+import Users from "./Users";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/users/:userid" element={<Users />}>
+          <Route path="info" element={<div>info</div>} />
+          <Route path="todos/:id?" element={<div>todos</div>} />
+          <Route path="posts/:id?" element={<div>posts</div>} />
+          <Route path="albums/:id?" element={<div>albums</div>} />
+        </Route>
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
---
+++ src/Users.js
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router-dom";
+
+function Users() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="info">Info</Link>
+        </li>
+        <li>
+          <Link to="albums">Albums</Link>
+        </li>
+      </ul>
+      <Outlet/>
+    </nav>
+  );
+}
+
+export default Users;
