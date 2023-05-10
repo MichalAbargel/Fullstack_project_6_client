e7acf162f41e4f52c61712294efcf52767655549
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-05-10 21:56:27 +0300

    info

--- src/Info.js
+++ src/Info.js
@@ -1,7 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Info() {
   const params = useParams();
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    //id or the whole user?
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (params.userid != user.id) {
+      localStorage.removeItem("user");
+      navigate("/login");
+    }
+    if (!user) {
+      setUser(user);
+    }
+  }, [user]);
+  if (!user) return null;
   return <div>info</div>;
 }
 
