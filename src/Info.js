179227d61e7fa635519683b3553727872acb7e65
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-05-16 16:26:51 +0300

    Loading

--- src/Info.js
+++ src/Info.js
@@ -17,7 +17,7 @@ function Info() {
         setUser(data.filter((user) => user.id == params.userid)[0]);
       }
     } catch (error) {
-      throw error;///////////////////?
+      throw error; ///////////////////?
     }
   };
 
@@ -25,23 +25,22 @@ function Info() {
     getUser();
   }, []);
 
+  if (!user) return <div>loading...</div>;
   return (
-    user && (
-      <div>
-        <h1>{user.name}</h1>
-        <p>Username: {user.username}</p>
-        <p>Email: {user.email}</p>
-        <p>
-          Address: {user.address.street}, {user.address.suite},{" "}
-          {user.address.city}, {user.address.zipcode}
-        </p>
-        <p>Phone: {user.phone}</p>
-        <p>Website: {user.website}</p>
-        <h2>Company: {user.company.name}</h2>
-        <p>Catchphrase: {user.company.catchPhrase}</p>
-        <p>BS: {user.company.bs}</p>
-      </div>
-    )
+    <div>
+      <h1>{user.name}</h1>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>
+        Address: {user.address.street}, {user.address.suite},{" "}
+        {user.address.city}, {user.address.zipcode}
+      </p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+      <h2>Company: {user.company.name}</h2>
+      <p>Catchphrase: {user.company.catchPhrase}</p>
+      <p>BS: {user.company.bs}</p>
+    </div>
   );
 }
 
