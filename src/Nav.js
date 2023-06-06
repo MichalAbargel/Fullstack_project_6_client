dde6debb81601551ae88c4d18c659dd23ae4309b
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-06-07 02:25:03 +0300

    a

--- src/Nav.js
+++ src/Nav.js
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
   /*--------------------
   Menu
   --------------------*/
-  //cathGPT
+  
   const Menu = ({items}) => {
     const $root = useRef()
     const $indicator1 = useRef()
